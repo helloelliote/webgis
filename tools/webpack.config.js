ea2f549928a42f3d5cace6a431549b48f44f9698
helloelliote <hello.elliote@gmail.com>
2020-09-22 15:54:58 +0900

    refactor: Bundle all map related styles using .scss imports function
    style.scss imports individual stylesheets from openlayers/Naver/kakao

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -162,6 +162,11 @@ function getEntryFiles() {
     { 'js/maps.bundle': ['./webpack/webgis/maps/index.js'] },
   );
 
+  // webgis
+  Object.assign(entries,
+    { 'css/maps.bundle': ['./webpack/webgis/maps/style.scss'] },
+  );
+ 
   // Custom styles
   Object.assign(entries,
     { 'css/style.custom.bundle': ['./webpack/webgis/metronic/style.scss'] },
