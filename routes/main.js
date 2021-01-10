115ffb6bc28ee55666d10a8b9d3b1d6154df1b36
helloelliote <hello.elliote@gmail.com>
2021-01-10 22:01:20 +0900

    chore: ExpressJS routers provide necessary API keys (ex. KAKAO_API_KEY)

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ export default {
     res.render('index', {
       _csrfToken: req.csrfToken(),
       title: '홈 |', // TODO: Fill in user organisation name
+      KAKAO_API_KEY: process.env.KAKAO_API_KEY,
     });
   },
 };
--- routes/service.js
+++ routes/service.js
@@ -41,6 +41,7 @@ export default {
     res.render('service-register', {
       _csrfToken: req.csrfToken(),
       title: '민원등록 |', // TODO: Fill in user organisation name
+      KAKAO_API_KEY: process.env.KAKAO_API_KEY,
     });
   },
 
@@ -88,6 +89,7 @@ export default {
     res.render('service-search', {
       _csrfToken: req.csrfToken(),
       title: '민원검색 |', // TODO: Fill in user organisation name
+      KAKAO_API_KEY: process.env.KAKAO_API_KEY,
     });
   },
 
