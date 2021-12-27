62662ff361c40aef60899b3b4033fae2cd3783ae
helloelliote <hello.elliote@gmail.com>
2021-12-27 10:33:02 +0900

    fix: Service registration number sequence (2)
    Set registration number sequencing to start from year 2022

--- tools/webpack/webgis/database/wfis47210a/service/di_yearly_counter.sql
+++ tools/webpack/webgis/database/wfis47210a/service/di_yearly_counter.sql
@@ -1,23 +1,25 @@
-CREATE OR REPLACE FUNCTION dj_yearly_counter() RETURNS INTEGER
-    LANGUAGE plpgsql
+CREATE FUNCTION dj_yearly_counter() RETURNS integer
+  LANGUAGE plpgsql
 AS
 $$
 DECLARE
-    current_year    CHAR(4);
-    current_rcv_num INT;
-    new_rcv_num     INT;
+  last_year    CHAR(4);
+  current_year INT;
+  last_rcv_num INT;
+  new_rcv_num     INT;
 BEGIN
-    current_rcv_num = rcv_num FROM wtt_wser_ma ORDER BY id DESC LIMIT 1;
-    current_year = LEFT(CAST(current_rcv_num AS CHAR(4)), 4);
+  last_rcv_num = rcv_num FROM wtt_wser_ma ORDER BY id DESC LIMIT 1;
+  last_year = LEFT(CAST(last_rcv_num AS CHAR(4)), 4);
+  current_year = EXTRACT(YEAR FROM CURRENT_DATE);
 
-    IF current_year = CAST(EXTRACT(YEAR FROM CURRENT_DATE) AS CHAR(4))
-    THEN
-        new_rcv_num = current_rcv_num + 1;
-    ELSE
-        new_rcv_num = CONCAT(EXTRACT(YEAR FROM CURRENT_DATE), '000001');
-    END IF;
+  IF current_year = 2021 OR last_year = CAST(current_year AS CHAR(4))
+  THEN
+    new_rcv_num = last_rcv_num + 1;
+  ELSE
+    new_rcv_num = CONCAT(current_year, '000001');
+  END IF;
 
-    RETURN CAST(new_rcv_num AS INT);
+  RETURN CAST(new_rcv_num AS INT);
 END;
 $$;
 
